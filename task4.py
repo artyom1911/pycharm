6aebad64aed64f6cb37f154b50ba42b61929d386
Artem Prudov <artyom1911@gmail.com>
2023-02-13 23:13:45 +0300

    finish task4

--- task4.py
+++ task4.py
@@ -4,3 +4,10 @@
 3 2 4 -> yes
 3 2 1 -> no"""
 
+n = int(input("Введите длинну = "))
+m = int(input("Введите ширину = "))
+k = int(input("Колличество ломтиков = "))
+if (n*m) % k == 0 and k != 1:
+    print ("Yes")
+else:
+    print("No")
